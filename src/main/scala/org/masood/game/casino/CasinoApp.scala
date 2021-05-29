581775a8209e70b0c70fa84d66a4e6627d4076f1
Masood Masaeli <masood@ngti.nl>
2021-05-29 10:50:04 +0200

    Move Main App to Scala

---
+++ src/main/scala/org/masood/game/casino/CasinoApp.scala
@@ -0,0 +1,11 @@
+package org.masood.game.casino
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+object CasinoApp extends App {
+  SpringApplication.run(classOf[CasinoApp])
+}
+
+@SpringBootApplication
+class CasinoApp
